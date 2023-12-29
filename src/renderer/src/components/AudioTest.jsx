a7df9bb582367652fe702fb7a9d8c476c07d6f0d
Edgar Rebollo <edgar-toyin1@hotmail.com>
2023-12-29 15:26:26 -0800

    XD

--- src/renderer/src/components/AudioTest.jsx
+++ src/renderer/src/components/AudioTest.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from 'react'
 import TypeOfTestMessage from './TypeOfTestMessage'
-import VideoPlay from './VideoPlay'
-import ModalHeadPhones from './ModalHeadPhones'
 import { getFilePath } from '../utilities/utilityFunctions'
 import useCountDown from './useCountDown'
+import ModalHeadPhones from './ModalHeadPhones'
 import AudioTestContent from './AudioTestContent'
 const SPARE_TIME = 15
 
@@ -19,22 +18,24 @@ export default function AudioTest() {
   const { secondsLeft, start, stop } = useCountDown()
 
   useEffect(() => {
+    // Set up event listener for device changes
+
     const file = getFilePath(
       '../../preload/The Weeknd - Save Your Tears (Official Music Video).mp4'
     )
-    getOutputAudioDevices()
     setVideoPath(file)
 
-    function handleDeviceChange() {
-      getOutputAudioDevices()
-    }
-
-    navigator.mediaDevices.addEventListener('devicechange', handleDeviceChange)
-
+    getDefaultOutAudio()
+    navigator.mediaDevices.ondevicechange = getDefaultOutAudio
     return () => {
-      navigator.mediaDevices.removeEventListener('devicechange', handleDeviceChange)
+      navigator.mediaDevices.ondevicechange = null
     }
   }, [])
+
+  async function getDefaultOutAudio() {
+    const output = await getOutputAudioDevices()
+    setMainOutput(output)
+  }
 
   // The first timer for the headphones test
   useEffect(() => {
@@ -44,24 +45,33 @@ export default function AudioTest() {
     console.log(secondsLeft)
   }, [secondsLeft])
 
+  useEffect(() => {
+    if (mainOutput) {
+      console.log(mainOutput)
+      console.log(device)
+      if (!mainOutput.includes(device)) {
+        setIsModalVisible(true)
+      } else {
+        setIsModalVisible(false)
+        setIsGuideVisible(true)
+      }
+    }
+  }, [mainOutput, device])
+
   async function getOutputAudioDevices() {
     try {
       const devices = await navigator.mediaDevices.enumerateDevices()
       const audioOutputDevices = devices.filter((device) => device.kind === 'audiooutput')
       const deviceType = audioOutputDevices.find((type) => type.label.includes('Default'))
-      setMainOutput(deviceType.label)
-      if (!deviceType.label.includes(device)) {
-        setIsModalVisible(true)
-      }
+      return deviceType.label
     } catch (err) {
-      setMainOutput(`${err.name}: ${err.message}`)
+      return `${err.name}: ${err.message}`
     }
   }
 
   function modalToRender() {
-    // CHECK IF THE CURRENT DEFAULT DEVICE IS THE SAME AS THE TESTING DEVICE, IF NOT SHOW A MODAL.
-    if (shouldStop) {
-      return <ModalHeadPhones title="Prueba de Audio" />
+    if (isModalVisible) {
+      return <ModalHeadPhones title={'pontelos'} />
     } else {
       return (
         <AudioTestContent
